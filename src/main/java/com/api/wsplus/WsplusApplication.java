38aac897182f3104291de105c678c4e24ccc5694
murilomunari <munarimurilo441@gmail.com>
2025-06-08 12:12:27 -0300

    fix: Resolvendo problema para deploy na AWS.

--- src/main/java/com/api/wsplus/WsplusApplication.java
+++ src/main/java/com/api/wsplus/WsplusApplication.java
@@ -2,12 +2,14 @@ package com.api.wsplus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.api.wsplus.Repository")
+@EntityScan(basePackages = "com.api.wsplus.Model")
 public class WsplusApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WsplusApplication.class, args);
 	}
-
 }
